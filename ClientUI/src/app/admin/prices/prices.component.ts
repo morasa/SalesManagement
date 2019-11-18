64142396888193bcb91168240c35fe450263a7b0
kranthi <kranthitimma@gmail.com>
2019-11-18 08:07:14 +0530

    price table added

--- ClientUI/src/app/admin/prices/prices.component.ts
+++ ClientUI/src/app/admin/prices/prices.component.ts
@@ -1,12 +1,12 @@
 import { Component, OnInit } from '@angular/core';
-
+import * as PriceDetatils from '../../common/config/prices.json';
 @Component({
   selector: 'app-prices',
   templateUrl: './prices.component.html',
   styleUrls: ['./prices.component.css']
 })
 export class PricesComponent implements OnInit {
-
+  price_list = PriceDetatils['default'];
   constructor() { }
 
   ngOnInit() {
